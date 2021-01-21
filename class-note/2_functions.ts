18cd1eb0908e577ade3be01635cce15b63c50c2d
pjaeyoung <devdodose@gmail.com>
2021-01-21 13:40:26 +0900

    함수의 타입을 정의하는 방식

---
+++ class-note/2_functions.ts
@@ -0,0 +1,14 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+// function sum(a: number, b: number) {
+//   return a + b;
+// }
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+  return "je";
+}
+
+// 함수의 타입을 정의하는 방식
+function sum(a: number, b: number): number {
+  return a + b;
+}
