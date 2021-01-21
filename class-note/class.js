cd9c390e22dfd3ff89b80d027ba0b3e73bdd5b3f
pjaeyoung <devdodose@gmail.com>
2021-01-21 16:04:11 +0900

    class 설명

---
+++ class-note/class.js
@@ -0,0 +1,13 @@
+// ES2015(ES6)
+
+class Person {
+  //클래스 로직
+  constructor(name, age) {
+    console.log("생성 되었습니다.");
+    this.name = name;
+    this.age = age;
+  }
+}
+
+var seho = new Person("세호", 30); // 생성 되었습니다.
+console.log(seho);
