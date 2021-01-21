2e1237e53404771021f8ed69bb1001df1bde3856
pjaeyoung <devdodose@gmail.com>
2021-01-21 13:57:41 +0900

    함수 옵셔널 파라미터

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -13,4 +13,11 @@ function sum2(a: number, b: number): number {
   return a + b;
 }
 
+sum2(10); // 선언한 인자 개수 보다 적게 쓰면 에러 발생
 sum2(10, 20, 30, 40); // 추가적인 인자를 넘기면 에러 발생, 파라미터 제한
+
+// 옴셔널 파라미터
+function log(a: string, b?: string) {}
+
+log("hello ts");
+log("hello ts", "abc");
