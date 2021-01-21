a81efc6278f2b2259b7732a3f4de28e345823bc0
pjaeyoung <devdodose@gmail.com>
2021-01-21 12:00:55 +0900

    jsdoc과 ts-check를 통해 javascript를 typescript처럼 사용하기

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,5 +1,19 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// sum(10, "20"); // 예상값 : 30 , 결과값: 1020
+
+/**
+ *
+ * @param {number} a 첫 번째 숫자
+ * @param {number} b 두 번째 숫자
+ * @returns {number} 덧셈 결과
+ */
+
+//@ts-check   -> 타입스크립트처럼 에러를 발생시킴
 function sum(a, b) {
   return a + b;
 }
 
-sum(10, "20"); // 예상값 : 30 , 결과값: 1020
+sum(10, "20"); // 커서를 가까이 하면 js docs 내용이 나타남
