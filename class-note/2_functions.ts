c5f5029ed3ec6344c8d5445c390a9c55378746ca
pjaeyoung <devdodose@gmail.com>
2021-01-21 13:52:56 +0900

    ts에서 function은 선언한 인자 개수를 넘기면 에러를 발생함

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -5,10 +5,12 @@
 
 // 함수의 반환 값에 타입을 정의하는 방식
 function add(): number {
-  return "je";
+  return 10;
 }
 
 // 함수의 타입을 정의하는 방식
-function sum(a: number, b: number): number {
+function sum2(a: number, b: number): number {
   return a + b;
 }
+
+sum2(10, 20, 30, 40); // 추가적인 인자를 넘기면 에러 발생, 파라미터 제한
