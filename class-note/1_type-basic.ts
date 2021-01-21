bf251da9a5c58382643a44656263a0477455b338
pjaeyoung <devdodose@gmail.com>
2021-01-21 12:30:26 +0900

    기본 타입 살펴보기

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,16 @@
+// JS 문자열 선언
+// var str = "hello";
+
+// TS 문자열 선언
+let str: string = "hello";
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Captin", "Thor", "Hulk", 10];
+let items: number[] = [1, 2, 3];
+
+let strNumArr1: Array<string | number> = ["a", 1, 2];
+let strNumArr2: (string | number)[] = ["a", 1, 2];
